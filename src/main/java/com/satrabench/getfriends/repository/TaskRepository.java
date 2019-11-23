3c94f72b13db838a435c421548f6cbd2807fa914
Andra <vasilacheandragabriela@gmail.com>
2019-11-23 19:34:03 +0200

    new tasks

--- src/main/java/com/satrabench/getfriends/repository/TaskRepository.java
+++ src/main/java/com/satrabench/getfriends/repository/TaskRepository.java
@@ -4,10 +4,6 @@ import com.satrabench.getfriends.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Id;
-import java.util.List;
-
-
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Integer> {
 }
